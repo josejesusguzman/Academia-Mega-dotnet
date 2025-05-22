b61cf5eef0949747d4aa9d02fd30845d8539b9d2
josejesusguzman <jose1824@live.com.mx>
2025-05-22 09:33:52 -0600

    Added Edit and Detail

--- PrimeraAPI/obj/Debug/net8.0/PrimeraAPI.AssemblyInfo.cs
+++ PrimeraAPI/obj/Debug/net8.0/PrimeraAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ed41c555704d49c91267e8d325a25114d37075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56264d741e7bad7af9052f8231a2df8c3faabca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
