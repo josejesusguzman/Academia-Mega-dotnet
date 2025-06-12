863beaebdd32e943dfc69d8b062947a81f9a3140
josejesusguzman <jose1824@live.com.mx>
2025-06-12 09:44:06 -0600

    Added: chatt completed

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -7,8 +7,9 @@ builder.Services.AddGrpc();
 builder.Services.AddSingleton<ChatHub>();
 
 var app = builder.Build();
+app.MapGrpcService<ChatServiceImp>();
 
-
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => 
+    "gRPC Chat - Bienvenido");
 
 app.Run();
